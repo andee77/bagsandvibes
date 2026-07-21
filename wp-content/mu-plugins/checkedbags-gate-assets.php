b4225200acf91edb77119fa195bc787411a0662b
andee77 <andee@prpconsulting.org>
2026-07-21 18:43:46 -0400

    Load Gate styles on Gate 11 travel rules page too

--- wp-content/mu-plugins/checkedbags-gate-assets.php
+++ wp-content/mu-plugins/checkedbags-gate-assets.php
@@ -30,7 +30,8 @@ function cb_is_gate_content_page() {
 			has_shortcode( $post->post_content, 'cb_gate_vacations' ) ||
 			has_shortcode( $post->post_content, 'cb_gate_boards' ) ||
 			has_shortcode( $post->post_content, 'cb_gate_payments' ) ||
-			has_shortcode( $post->post_content, 'cb_gate_gallery' )
+			has_shortcode( $post->post_content, 'cb_gate_gallery' ) ||
+			has_shortcode( $post->post_content, 'cb_gate_rules' )
 		) ) {
 			return true;
 		}
